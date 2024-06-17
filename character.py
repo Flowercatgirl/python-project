52195bfa20ec9f9a7698a9b11eacce6bcc5dd6db
Flowercatgirl <oksana.pankeviciene@gmail.com>
2024-06-17 14:28:41 +0100

    Substitute damage attribute by a weapon one

--- character.py
+++ character.py
@@ -1,14 +1,18 @@
+from weapon import fists
+
 class Character:
     # Creates a Character blueprint
 
-    def __init__(self, name: str, health: int, damage: int) -> None:
+    def __init__(self, name: str, health: int) -> None:
         '''
         Initialize a new Character instance
         '''
         self.name = name
         self.health = health
         self.health_max = health
-        self.damage = damage
+
+        #Giving all Character objects fists as a weapon
+        self.weapon = fists
 
     def attack(self, target) -> None:
         '''
@@ -16,6 +20,6 @@ class Character:
         '''
 
         # Health attribute of the target will be reduced by the damage of the attacker
-        target.health -=self.damage
+        target.health -=self.weapon.damage
         # To avoid going below zero I am using the max function
         target.health = max(target.health, 0)
